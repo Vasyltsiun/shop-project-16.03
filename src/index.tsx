4044951f40066f64f3998453c0a74fd209c25f08
Oleksandr <vasiltsun.oleksandr@gmail.com>
2024-06-30 20:12:09 +0300

    added funtion declaration

--- src/index.tsx
+++ src/index.tsx
@@ -1,39 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-/*const h1 = React.createElement('h1', { id: 'title' }, 'Hello app')
-const p = React.createElement('p', null, 'Blalalala')
-
-const app = (
-    <div>
-        {h1}
-        {p}
-    </div>
-)*/
-
-const h1 = <h1 id="title">Hello App</h1>
-const p = (
-    <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde
-        aspernatur odit distinctio porro optio temporibus tempora dolore soluta.
-        Voluptatem rem dolore placeat commodi nemo voluptate iste optio
-        inventore ipsam illum?
-    </p>
-)
-const ul = (
-    <ul>
-        <li>List item 1</li>
-        <li>List item 2</li>
-        <li>List item 3</li>
-    </ul>
-)
-
-const app = (
-    <div>
-        {h1}
-        {p}
-        {ul}
-    </div>
-)
+//funtion declaration
+function Title() {
+    return <h1>Hello Title Component</h1>
+}
+function Component() {
+    return (
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+            doloremque architecto vitae neque molestiae, ea illo provident
+            voluptate, earum necessitatibus sit beatae eos quas corporis
+            explicabo sunt assumenda autem quam.
+        </p>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{app}</React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <Title />
+        <Component />
+    </React.StrictMode>
+)
