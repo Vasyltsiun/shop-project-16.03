9f7fea713eb9a2788bb207f6ad7157481962b2a5
Oleksandr <vasiltsun.oleksandr@gmail.com>
2024-06-30 19:44:22 +0300

    added alement without

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+const h1 = React.createElement('h1', { id: 'title' }, 'Hello app')
+const p = React.createElement('p', null, 'Blalalala')
+
+const app = (
+    <div>
+        {h1}
+        {p}
+    </div>
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+root.render(<React.StrictMode>{app}</React.StrictMode>)
