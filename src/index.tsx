9f297b59a2c948f7bbd6640e2e38934e6f7fcbc0
Oleksandr <vasiltsun.oleksandr@gmail.com>
2024-06-30 20:26:54 +0300

    added second component to content

--- src/index.tsx
+++ src/index.tsx
@@ -3,21 +3,27 @@ import ReactDOM from 'react-dom/client'
 //arrow Function
 const Title = () => <h1>Hello Title Component</h1>
 
-function Component() {
-    return (
+const Content = () => (
+    <div>
         <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae
             doloremque architecto vitae neque molestiae, ea illo provident
             voluptate, earum necessitatibus sit beatae eos quas corporis
             explicabo sunt assumenda autem quam.
         </p>
-    )
-}
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia alias
+            doloribus voluptatem provident veritatis at nemo quibusdam
+            accusantium, illum facilis deleniti architecto culpa porro optio a
+            repellendus possimus nulla mollitia!
+        </p>
+    </div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
         <Title />
-        <Component />
+        <Content />
     </React.StrictMode>
 )
