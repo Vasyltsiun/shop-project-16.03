f6187bd90a2ba36c0234edcddf829478981e3ace
Oleksandr <vasiltsun.oleksandr@gmail.com>
2024-07-14 19:51:16 +0300

    added Logo and Menu

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -1,10 +1,14 @@
 import Header from './Header'
 import CssBaseline from '@mui/material/CssBaseline'
+import Main from './Main'
+import Footer from './Footer'
 const App = () => {
     return (
         <>
             <CssBaseline />
             <Header />
+            <Main />
+            <Footer />
         </>
     )
 }
--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,12 +1,9 @@
-import React from 'react'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button'
-import IconButton from '@mui/material/IconButton'
-import MenuIcon from '@mui/icons-material/Menu'
 import { Container } from '@mui/material'
-import logo from '../assets/logo.svg'
+import Logo from '../components/Logo/Logo'
+import Menu from '../components/Menu/Menu'
+
 type Props = {}
 
 const Header = (props: Props) => {
@@ -16,26 +13,8 @@ const Header = (props: Props) => {
             <AppBar position="static" sx={{ backgroundColor: 'green' }}>
                 <Container>
                     <Toolbar>
-                        <IconButton
-                            size="large"
-                            edge="start"
-                            color="inherit"
-                            aria-label="menu"
-                            sx={{ mr: 2 }}
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Typography
-                            variant="h6"
-                            component="div"
-                            sx={{ flexGrow: 1 }}
-                        >
-                            <img src={logo} alt="" />
-                        </Typography>
-                        <Button color="inherit">Home</Button>
-                        <Button color="inherit">Products</Button>
-                        <Button color="inherit">Payment</Button>
-                        <Button color="inherit">Cart</Button>
+                        <Logo />
+                        <Menu />
                     </Toolbar>
                 </Container>
             </AppBar>
