4dcf65d074c7416a8fbc2fc6bd8b397c46b0cf99
Oleksandr <vasiltsun.oleksandr@gmail.com>
2024-08-11 20:20:41 +0300

    added content to CartPage.tsx

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -37,7 +37,10 @@ const App = () => {
                     path="/"
                     element={<Home addProductToCart={addProductToCart} />}
                 />
-                <Route path="cart" element={<CartPage />} />
+                <Route
+                    path="cart"
+                    element={<CartPage productsInCart={productsInCart} />}
+                />
                 <Route path="payment" element={<PaymentPage />} />
                 <Route path="products" element={<ProductPage />} />
             </Routes>
